87f97d65288ee47b449cffd99f6b624e5cc879b3
Ibbo7 <147712889+Ibbo7@users.noreply.github.com>
2025-04-02 12:17:17 +0200

    installation

---
+++ docs/_app/immutable/nodes/3.DNo6C39I.js
@@ -0,0 +1,1 @@
+import{t as o,a}from"../chunks/BieCTM-P.js";import"../chunks/CapjwU6s.js";import{n as p}from"../chunks/DNC9FTi1.js";var r=o('<h1>Welcome to about</h1> <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>',1);function i(t){var e=r();p(2),a(t,e)}export{i as component};
